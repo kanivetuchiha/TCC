61dcc36d61b08edd19b9c56cd9b3e2dfd7f8da61
kanivetuchiha <erickazambuja.ms@gmail.com>
2025-11-14 11:32:33 -0400

    update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home.jsx";
 import Cadastrar from "./pages/Cadastro.jsx"
 import Sobre from "./pages/Sobre.jsx"
 import Galeria from "./pages/Galeria.jsx"
-import Login from "./pages/Login.jsx"
+
 
 function App() {
   return (
@@ -12,7 +12,6 @@ function App() {
       <Route path="/cadastro" element={<Cadastrar />} />
       <Route path="/sobre" element={<Sobre />} />
       <Route path="/galeria" element={<Galeria />} />
-      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
--- front/src/pages/Login.jsx
+++
@@ -1,3 +0,0 @@
-export default function Login (){
-    <h1>login</h1>
-}
